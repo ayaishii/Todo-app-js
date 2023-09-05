757e17bd4c68f77da274c4bc63222889e0a28b52
ishii aya <ayaishiiii@gmail.com>
2023-09-05 09:28:45 +0000

    Update 2 files

--- src/index.mjs
+++ src/index.mjs
@@ -1,2 +1,7 @@
 import "./styles.css";
 
+const onclickAdd = () => {
+    alert("Hello, World!"); // alert() の中にメッセージを追加しました
+}
+
+document.getElementById("add-button").addEventListener("click", onclickAdd);
